745387869e8ac9ed92afb0ceae3c9c9c649a91a6
lamdaV <dav_lam@ymail.com>
2015-03-13 13:21:55 -0400

    Added more problems to solve and added test cases for each.

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,15 +1,68 @@
 package main.java;
 
-
 public class HelloWorld {
 
 	/**
 	 * Make the classic HelloWorld program in Java.
-	 *
+	 * 
 	 * @return the statement "Hello World"
 	 */
 	public static String hello() {
 		return " ";
 	}
 
+	/**
+	 * Make a basic summation function between two integers.
+	 * 
+	 * @param x
+	 * @param y
+	 * @return the sum of x and y.
+	 */
+	public static int sum(int x, int y) {
+		return 0;
+	}
+
+	/**
+	 * Return the distance between the two given numbers.
+	 * 
+	 * Hint: Explore the Math (Use Math(dot)) and find useful functions to
+	 * solve.
+	 * 
+	 * @param x1
+	 * @param y1
+	 * @param x2
+	 * @param y2
+	 * @return the distance between the given coordinates.
+	 */
+	public static int distance(int x1, int y1, int x2, int y2) {
+		return 0;
+	}
+
+	/**
+	 * Return the sum of the values within the given array.
+	 * 
+	 * Hint: Use a for loop. If you are daring, use a while loop. If you are an
+	 * expert, use recursion.
+	 * 
+	 * @param listOfInts
+	 *            an array of integers.
+	 * @return sum of of array values.
+	 */
+	public static int summation(int[] listOfInts) {
+		return 0;
+	}
+
+	/**
+	 * Combine the two given strings.
+	 * 
+	 * Example: If the strings "Hello" and "World" were given, the result would
+	 * be "HelloWorld."
+	 * 
+	 * @param a
+	 * @param b
+	 * @return the combined strings.
+	 */
+	public static String combineString(String a, String b) {
+		return "";
+	}
 }
--- src/test/java/HelloWorldTest.java
+++ src/test/java/HelloWorldTest.java
@@ -2,6 +2,9 @@ package test.java;
 
 
 import static org.junit.Assert.assertEquals;
+
+import java.util.Random;
+
 import main.java.HelloWorld;
 
 import org.junit.Test;
@@ -9,8 +12,52 @@ import org.junit.Test;
 public class HelloWorldTest {
 
 	@Test
-	public void test() {
+	public void testHello() {
 		assertEquals("Hello World", HelloWorld.hello());
 	}
-
+	
+	@Test
+	public void testSum() {
+		assertEquals(10, HelloWorld.sum(7, 3));
+		assertEquals(4, HelloWorld.sum(2, 2));
+		assertEquals(90, HelloWorld.sum(40, 50));
+		assertEquals(21, HelloWorld.sum(14, 7));
+		assertEquals(2, HelloWorld.sum(-4, 6));
+		assertEquals(103, HelloWorld.sum(70, 33));
+		assertEquals(10, HelloWorld.sum(5, 5));
+		assertEquals(0, HelloWorld.sum(0, 0));
+		assertEquals(0, HelloWorld.sum(1, -1));
+		assertEquals(Integer.MAX_VALUE, HelloWorld.sum(Integer.MAX_VALUE, 0));
+	}
+	
+	@Test
+	public void testDistance() {
+		assertEquals(0, HelloWorld.distance(0, 0, 0, 0));
+		assertEquals(6, HelloWorld.distance(3, 3, 9, 3));
+		assertEquals(31, HelloWorld.distance(30, 2, -1, 8));
+		assertEquals(2, HelloWorld.distance(1, 1, 2, 3));
+	}
+	
+	@Test
+	public void testSummation() {
+		int[] listOfInts = new int[10];
+		Random rand = new Random();
+		int sum = 0;
+		for (int i = 0; i < listOfInts.length; i++) {
+			listOfInts[i] = rand.nextInt(40);
+		}
+		
+		for (int k = 0; k < listOfInts.length; k++) {
+			sum = sum + listOfInts[k];
+		}
+		
+		assertEquals(sum, HelloWorld.summation(listOfInts));		
+	}
+	
+	@Test
+	public void testCombineString() {
+		assertEquals("HelloWorld", HelloWorld.combineString("Hello", "World"));
+		assertEquals("RainFall", HelloWorld.combineString("Rain", "Fall"));
+		assertEquals(":-)", HelloWorld.combineString(":-", ")"));
+	}
 }
